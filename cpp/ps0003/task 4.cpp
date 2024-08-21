907c70a6169f20a0b91a3c4c3e4ffe058e7f82ca
Youssef22878 <ibrahimalmahdi19752019@gmail.com>
2024-08-21 03:17:38 +0300

    Add files via upload

---
+++ cpp/ps0003/task 4.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "inter a number \n";
+    std::cin >> number;
+    if (number % 2 == 0)
+        std::cout << "even\n";
+    else
+        std::cout << "odd\n";
+        return 0;
+
+}
+
